0447aecc634f81feb3333551610f3b59fce2c48a
Leandro Simões <lesimoes0@gmail.com>
2021-04-25 11:14:03 -0300

    1- criado modelo basico do card

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import CardQuote from './components/card/card.component';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <CardQuote 
+        quote='Não caia no golpe!'
+      />
     </View>
   );
 }
