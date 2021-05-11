977f1e1e9de706afbcc955ae9255435da2cc1ee2
Leandro Simões <lesimoes0@gmail.com>
2021-05-10 22:24:21 -0300

    adicionado botao de like com contagem

--- components/card/card.component.jsx
+++ components/card/card.component.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { Button, Card, Paragraph, Avatar } from 'react-native-paper';
 
@@ -35,7 +35,51 @@ const styles = StyleSheet.create({
     }
 });
 
-const CardQuote = ({ quote, author, profileImg }) => {
+const CardQuote = ({ quote, author, profileImg, likes, likedUser }) => {
+
+    const [likeButton, setLikeButton] = useState(likedUser);
+    const [likeClass, setLikeClass] = useState("heart-outline");
+    const [qtdLikes, setQtdLikes] = useState(likes);
+
+    // Renderiza as curtidas na primeira vez
+    useEffect(() => {
+        if (likeButton) {
+            setLikeClass("cards-heart")
+        } else {
+            setLikeClass("heart-outline")
+        }
+    }, [])
+
+    const handleLike = () => {
+        if (likeButton) {
+            setLikeClass("heart-outline")
+            setQtdLikes(() => qtdLikes - 1)
+        } else {
+            setLikeClass("cards-heart")
+            setQtdLikes(() => qtdLikes + 1)
+        }
+        setLikeButton(() => !likeButton)
+    }
+
+
+    const handleShare = async () => {
+        if(Platform.OS !== 'web') {
+            const result = await Share.share({
+                message: `${text} - ${author}`,
+            })
+            if (result.action === Share.sharedAction) {
+                if (result.activityType) {
+                    // NADA
+                } else {
+                    console.log('compartilhado')
+                }
+            }
+        } else {
+            alert('Meu parcero, se liga que essa função é só no mobile...')
+        }
+    }
+
+    
     return (
         <Card>
             <Card.Title 
@@ -50,15 +94,17 @@ const CardQuote = ({ quote, author, profileImg }) => {
             </Card.Content>
             <Card.Actions style={styles.actions}>
                 <Button 
-                icon="share-variant" 
-                color='#541616'
-                labelStyle={{ fontSize: 24 }}
+                    icon="share-variant"
+                    color="#541616"
+                    labelStyle={{fontSize: 24}}
+                    onPress={handleShare}
                 />
                 <Button 
-                icon="star-outline" 
-                color='#541616'
-                labelStyle={{ fontSize: 24 }}
-                />
+                    icon={likeClass}
+                    color="#541616"
+                    labelStyle={{fontSize: 24}}
+                    onPress={handleLike}
+                >{qtdLikes}</Button>
             </Card.Actions>
         </Card>
     )
